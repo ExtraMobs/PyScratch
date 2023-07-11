1c40808fd744b89b0752088531e52eb1e7d80942
Mario Cesar Silva Elvira <mariocesarelvira@hotmail.com>
2023-07-11 03:30:34 -0300

    reestrutura dos arquivos

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ class PyScratch:
     def __init__(self):
         self.load_assets()
 
+        Window.set_title("PyScratch")
         Window.set_size((1280, 720))
         Display.update_display_from_window()
         Engine.set_scene(MainScene())
 
     def load_assets(self):
-        Resources.Files.add_from_path("MonoFonto", "monofonto rg.otf")
+        Resources.Files.add_from_path("MonoFonto", "assets/monofonto rg.otf")
 
     def start(self):
         Engine.start_loop()
--- scenes/mainscene.py
+++ scenes/mainscene.py
@@ -1,16 +1,13 @@
 from gameengine.basescene import BaseScene
 from gameengine.engine import Engine
 from gameengine.resources import Resources
-from text import Text, TEXT_STATIC_MODE
+from children.text import Text, TEXT_STATIC_MODE
 
 
 class MainScene(BaseScene):
     def __init__(self) -> None:
         super().__init__()
         self.color_fill = (200, 200, 200)
-        
-        text = Text(TEXT_STATIC_MODE, Resources.Fonts.get_from_file_buffer("MonoFonto", 50), (0,0,0), None, "Teste")
-        self.add_children(text)
 
     def update(self):
         super().update()
