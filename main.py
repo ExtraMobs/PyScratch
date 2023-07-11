042f9445833d6adb7bb160ec6e6265be81c50746
Mario Cesar Silva Elvira <mariocesarelvira@hotmail.com>
2023-07-11 03:25:02 -0300

    testes com texto
    teste com a nova classe de texto

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from gameengine.display import Display
 from gameengine.engine import Engine
+from gameengine.resources import Resources
 from gameengine.window import Window
 from scenes.mainscene import MainScene
 
 
 class PyScratch:
     def __init__(self):
+        self.load_assets()
+
         Window.set_size((1280, 720))
         Display.update_display_from_window()
         Engine.set_scene(MainScene())
+
+    def load_assets(self):
+        Resources.Files.add_from_path("MonoFonto", "monofonto rg.otf")
 
     def start(self):
         Engine.start_loop()
--- text.py
+++ text.py
@@ -1,11 +1,26 @@
-from gameengine.basechild import BaseChild
 import enum
 
-TEXT_STATIC = enum.auto()
+import pygame
+
+from gameengine.basechild import BaseChild
+
+TEXT_STATIC_MODE = enum.auto()
+
 
 class Text(BaseChild):
-    def __init__(self, mode, font) -> None:
+    def __init__(self, mode, font: pygame.font.Font, fg, bg=None, text="") -> None:
         super().__init__()
-        
-        if mode == TEXT_STATIC:
-            
+        self.font = font
+        self.mode = mode
+        self.fg = fg
+        self.bg = bg
+
+        self.set_text(text)
+
+    def set_text(self, text):
+        self.text = text
+        self.surface = self.font.render(self.text, True, self.fg, self.bg)
+        if self.rect is None:
+            self.rect = self.surface.get_frect()
+        else:
+            self.rect.size = self.surface.get_size()
