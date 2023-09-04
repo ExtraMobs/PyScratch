e0fd79b4023909f806c44ec14d9225ca524e7777
Mario Cesar Silva Elvira <50765958+ExtraMobs@users.noreply.github.com>
2023-09-04 00:37:02 -0300

    'gameengine' atualizado

---
+++ gameengine/nodes/shadingnode.py
@@ -0,0 +1,30 @@
+import pygame
+
+from .node import Node
+
+
+class ShadingManager(Node):
+    def draw(self, surf: pygame.Surface) -> None:
+        for shader in self.children:
+            shader.draw(
+                pygame.surfarray.pixels2d(surf),
+                pygame.surfarray.pixels3d(surf),
+                pygame.surfarray.pixels_alpha(surf),
+            )
+
+
+class ShadingNode(Node):
+    def __init__(self, *children) -> None:
+        super().__init__(*children)
+        self.shader_manager = ShadingManager()
+
+    def add_shader(self, *new_shaders) -> None:
+        self.shader_manager.add_children(*new_shaders)
+
+    def update(self) -> None:
+        super().update()
+        self.shader_manager.update()
+
+    def draw(self) -> None:
+        super().draw()
+        self.shader_manager.draw(self.surface)
--- scenes/mainscene.py
+++ scenes/mainscene.py
@@ -1,29 +1,7 @@
-from children.menubar import MenuBar
-from children.option import Option
-from gameengine.basescene import BaseScene
-from gameengine.engine import Engine
+from gameengine.nodes.scene import Scene
 
 
-class MainScene(BaseScene):
+class MainScene(Scene):
     def __init__(self) -> None:
         super().__init__()
         self.bg = (200, 200, 200)
-
-        menubar_test = MenuBar(
-            {
-                "a1": 
-                    {"b1": lambda: print("hello world"),
-                     "b2": lambda: print("hello world")},
-                "a2": 
-                    {"b1": lambda: print("hello world"), 
-                     "b2": lambda: print("hello world")},
-            },
-            (self.surface.get_width(), 50),
-        )
-        self.add_children(menubar_test)
-
-    def update(self):
-        super().update()
-
-        if Engine.request_quit:
-            Engine.system_exit()
