e5e8cb7bb66662465d4fac92e2d6740ac492d9a2
Mario Cesar Silva Elvira <50765958+ExtraMobs@users.noreply.github.com>
2023-09-04 15:45:04 -0300

    'prepare_surface' pronto
    função que prepara a Surface de cada opção do menubar

--- menubar.py
+++ menubar.py
@@ -1,32 +1,61 @@
+import pygame
+
 from gameengine import resources
 from gameengine.nodes.graphicnode import GraphicNode
 from gameengine.nodes.node import Node
 
 
-class TreeNode(GraphicNode):
-    def __init__(self):
-        super().__init__(resources.surface.new((1, 1)))
-
-
-class Option(TreeNode):
+class Option(GraphicNode):
     COLORS = {
         "idle": (150, 150, 150),
         "selected": (100, 100, 100),
     }
 
     DEFAULT_WIDTH = 200
+    DEFAULT_HEIGHT = 50
     PADING = 10
 
+    surface_idle = None
+    surface_selected = None
+
     def __init__(self, data_to_display=None):
-        super().__init__()
-        self.data = data_to_display
+        super().__init__(resources.surface.new((1, 1)))
+        self.text = str(data_to_display)
 
         self.prepare_surface()
 
     def prepare_surface(self):
         font = resources.fonts.get_from_file_buffer(20, "MonoFonto")
-        print(font)
-        # self.surface =
+        surface_text = font.render(self.text, True, (0, 0, 0))
+        surface = resources.surface.new((self.DEFAULT_WIDTH, self.DEFAULT_HEIGHT))
+
+        pretended_width = self.DEFAULT_WIDTH - 2 * self.PADING
+        pretended_height = self.DEFAULT_HEIGHT - 2 * self.PADING
+
+        surface_text = surface_text.subsurface(
+            pygame.Rect(
+                0,
+                0,
+                pretended_width
+                if surface_text.get_width() + 2 * self.PADING > self.DEFAULT_WIDTH
+                else surface_text.get_width(),
+                pretended_height
+                if surface_text.get_height() + 2 * self.PADING > self.DEFAULT_HEIGHT
+                else surface_text.get_height(),
+            )
+        )
+
+        blit_pos = (self.PADING, self.PADING)
+
+        self.surface_idle = surface.copy()
+        self.surface_idle.fill(self.COLORS["idle"])
+        self.surface_idle.blit(surface_text, blit_pos)
+
+        self.surface_selected = surface.copy()
+        self.surface_selected.fill(self.COLORS["selected"])
+        self.surface_selected.blit(surface_text, blit_pos)
+
+        self.surface = self.surface_idle
 
 
 class Tree(Node):
@@ -42,18 +71,24 @@ class Tree(Node):
             else:
                 option.add_children(new_option)
 
+            # if callable(value):
+
 
 class MenuBar(GraphicNode):
-    OPTION_HEIGHT = 50
     COLOR = (170, 170, 170)
 
     def __init__(self):
         width = self.program.window.width
 
-        super().__init__(resources.surface.new((width, self.OPTION_HEIGHT)))
+        super().__init__(resources.surface.new((width, Option.DEFAULT_HEIGHT)))
 
         self.surface.fill(self.COLOR)
 
         self.add_children(
-            Tree({"a1": {"b1": [], "b2": []}, "a2": {"b1": [], "b2": []}})
+            Tree(
+                {
+                    "aaaaaaaaaaaaaaaaaa1": {"b1": [], "b2": []},
+                    "a2": {"b1": [], "b2": []},
+                }
+            )
         )
