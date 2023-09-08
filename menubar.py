65139e723d6b4f60008948973a1b447b163b235c
Mario Cesar Silva Elvira <50765958+ExtraMobs@users.noreply.github.com>
2023-09-08 16:53:12 -0300

    'TreeNode' recursivo part 5
    Problema de prioridade recursiva resolvido, está aparecendo conforme é declarado e se comportando como esperado de um 'MenuBar'.

--- menubar.py
+++ menubar.py
@@ -11,6 +11,7 @@ class Option(GraphicNode):
     COLORS = {
         "idle": (150, 150, 150),
         "selected": (100, 100, 100),
+        "expansible": (150, 100, 150),
     }
 
     top = False
@@ -24,13 +25,13 @@ class Option(GraphicNode):
 
     function = None
 
+    __tree_selected = False
+
     def __init__(self, data_to_display=None):
         super().__init__(
             resources.surface.new((self.DEFAULT_WIDTH, self.DEFAULT_HEIGHT))
         )
         self.text = str(data_to_display)
-
-        self.prepare_surface()
 
     @property
     def selected(self):
@@ -39,6 +40,11 @@ class Option(GraphicNode):
     @property
     def idle(self):
         return self.surface is self.surface_idle
+
+    @property
+    def tree_selected(self):
+        children_selected = bool(sum([child.tree_selected for child in self.children]))
+        return self.__tree_selected or children_selected
 
     def set_active_children(self, state: bool):
         for child in self.children:
@@ -65,25 +71,30 @@ class Option(GraphicNode):
             if index == 0:
                 if self.parent.top:
                     self.rect.x = self.parent.rect.x
+                    self.rect.y = self.parent.rect.bottom
                 else:
                     self.rect.x = self.parent.rect.right
-                self.rect.y = self.parent.rect.bottom
+                    self.rect.y = self.parent.rect.y
             else:
                 last_opt = self.parent.children[index - 1]
                 self.rect.x = last_opt.rect.x
                 self.rect.y = last_opt.rect.bottom
+
         if self.rect.collidepoint(self.program.devices.mouse.pos):
             self.surface = self.surface_selected
-        elif self.selected:
+            self.__tree_selected = True
+        else:
+            self.__tree_selected = False
+
+        if self.selected and not self.tree_selected:
             self.surface = self.surface_idle
+
         if self.selected:
             self.set_active_children(True)
             self.set_visible_children(True)
         elif self.idle:
             self.set_active_children(False)
             self.set_visible_children(False)
-        if "b" in self.text:
-            print(self.path, self.text)
 
     def prepare_surface(self):
         font = resources.fonts.get_from_file_buffer(20, "MonoFonto")
@@ -107,13 +118,29 @@ class Option(GraphicNode):
 
         blit_pos = (self.PADING, self.PADING)
 
-        self.surface_idle = self.surface.copy()
-        self.surface_idle.fill(self.COLORS["idle"])
-        self.surface_idle.blit(surface_text, blit_pos)
+        self.surface_idle = surface = self.surface.copy()
+        surface.fill(self.COLORS["idle"])
+        if len(self.children) > 0:
+            marker_width = 10
+            surface.fill(
+                self.COLORS["expansible"],
+                pygame.Rect(
+                    -marker_width % self.rect.width, 0, marker_width, self.rect.height
+                ),
+            )
+        surface.blit(surface_text, blit_pos)
 
-        self.surface_selected = self.surface.copy()
-        self.surface_selected.fill(self.COLORS["selected"])
-        self.surface_selected.blit(surface_text, blit_pos)
+        self.surface_selected = surface = self.surface.copy()
+        surface.fill(self.COLORS["selected"])
+        if len(self.children) > 0:
+            marker_width = 5
+            surface.fill(
+                self.COLORS["expansible"],
+                pygame.Rect(
+                    -marker_width % self.rect.width, 0, marker_width, self.rect.height
+                ),
+            )
+        surface.blit(surface_text, blit_pos)
 
         self.surface = self.surface_idle
 
@@ -143,8 +170,9 @@ class Tree(Node):
                 new_option.function = value
             elif type(value) is dict:
                 self.set_data_from_nested_tree(
-                    value, new_option, new_option if top_parent is None else top_parent
+                    value, new_option, new_option if new_option.top else top_parent
                 )
+            new_option.prepare_surface()
 
 
 class MenuBar(GraphicNode):
@@ -153,12 +181,18 @@ class MenuBar(GraphicNode):
     def __init__(self):
         width = self.program.window.width
 
-        super().__init__(resources.surface.new(size := (width, Option.DEFAULT_HEIGHT)))
+        super().__init__(resources.surface.new((width, Option.DEFAULT_HEIGHT)))
         self.add_children(
             Tree(
                 {
-                    "a1": {"b1": [], "b2": []},
-                    "a2": {"b1": [], "b2": []},
+                    "a1": {
+                        "b1": {"c1": lambda: print("c1"), "c2": lambda: print("c2")},
+                        "b2": {"c1": lambda: print("c1"), "c2": lambda: print("c2")},
+                    },
+                    "a2": {
+                        "b1": {"c1": lambda: print("c1"), "c2": lambda: print("c2")},
+                        "b2": {"c1": lambda: print("c1"), "c2": lambda: print("c2")},
+                    },
                 }
             )
         )
