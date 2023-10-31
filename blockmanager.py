5464ad9d54b33bf36e123d7000a8da822d866128
Mario Cesar Silva Elvira <50765958+ExtraMobs@users.noreply.github.com>
2023-10-31 01:51:14 -0300

    Update blockmanager.py

--- blockmanager.py
+++ blockmanager.py
@@ -3,15 +3,10 @@ import pygame
 from gameengine.nodes.node import Node
 
 
-class BlockManager(Node):
-    drag_block = None
-
-    def __init__(self) -> None:
-        super().__init__()
+class DragBlockManager:
+    def __init__(self, block_manager):
+        self.block_manager = block_manager
         self.drag_block = None
-
-    def add_blocks(self, *blocks):
-        super().add_children(*blocks)
 
     def update(self):
         if self.program.devices.mouse.get_pressed_up_in_frame(pygame.BUTTON_LEFT):
@@ -24,4 +19,31 @@ class BlockManager(Node):
             self.drag_block.rect.y = (
                 self.program.devices.mouse.pos.y - self.drag_block.drag_offset.y
             )
+
+    @property
+    def program(self):
+        return self.block_manager.program
+
+
+class BlockManager(Node):
+    drag_block_manager = None
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.drag_block_manager = DragBlockManager(self)
+
+    def add_blocks(self, *blocks):
+        super().add_children(*blocks)
+
+    def update(self):
+        self.drag_block_manager.update()
         super().update()
+
+    # temp
+    @property
+    def drag_block(self):
+        return self.drag_block_manager.drag_block
+
+    @drag_block.setter
+    def drag_block(self, value):
+        self.drag_block_manager.drag_block = value
