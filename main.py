3b84d22e818ac636083e6c58c2cac388db49d7dc
Mario Cesar Silva Elvira <50765958+ExtraMobs@users.noreply.github.com>
2024-02-10 08:07:02 -0300

    melhor organização
    a partir dos próximos commits vou descrever em inglês

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 import pygame
 
-window = pygame.display.set_mode((720, 405))
 
-clock = pygame.time.Clock()
+class ProgramLoop:
+    def __init__(self, window_manager, framerate_manager):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    raise SystemExit(0)
 
-framerate = 60
+            window_manager.update_display()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            raise SystemExit(0)
+            framerate_manager.process()
 
-    clock.tick(framerate)
+
+class WindowManager:
+    def __init__(self, pygame_window):
+        self.pygame_window = pygame_window
+        self.display_surface = pygame_window.get_surface()
+
+    def update_display(self):
+        self.pygame_window.flip()
+
+
+class FramerateManager:
+    @property
+    def framerate(self):
+        self.clock.get_fps()
+
+    def __init__(self, target_framerate):
+        self.clock = pygame.time.Clock()
+        self.target_framerate = 60
+
+    def process(self):
+        self.clock.tick(self.target_framerate)
+
+
+ProgramLoop(WindowManager(pygame.Window("PyScratch", (720, 405))), FramerateManager(60))
